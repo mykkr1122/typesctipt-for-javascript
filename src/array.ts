df94d76c717f1a62033acce8b49d88dd4a3165f8
mykkr1122 <orangechocola22@icloud.com>
2024-12-19 17:14:52 +0900

    array

---
+++ src/array.ts
@@ -0,0 +1,17 @@
+export {};
+
+let numbers: number[] = [1, 2, 3];
+
+let numbers2: Array<number> = [1, 2, 3];  // interface
+
+let strings: string[] = ['TypeScript', 'JavaScript', 'CoffeeScript'];
+
+let string2: Array<string> = ['Tokyo', 'Nagoya', 'Sapporo'];
+
+// 2次元配列
+let twoDimansionalArray: number[][] = [
+    [50, 100],
+    [150, 300]
+];
+
+let arrays: (string | number | boolean)[] = [1, false, 'Japan'];
---
+++ src/number-string.ts
@@ -0,0 +1,12 @@
+export {};
+
+let year: number = 1995;
+// year = true;
+console.log(year);
+
+let age: number = 0x2b;
+console.log(age);
+
+let name: string = 'Ham';
+// name = 10;
+console.log(name);
